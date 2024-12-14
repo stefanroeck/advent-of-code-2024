e75cc1181352b928e8bcca0509901ce02706d1cb
Stefan Roeck <stefan.roeck@gmail.com>
2024-12-14 23:24:26 +0100

    extract file loading to util

--- src/main/kotlin/day03/Main.kt
+++ src/main/kotlin/day03/Main.kt
@@ -1,9 +1,9 @@
 package day03
 
+import util.FileUtil
+
 fun main() {
-    val file = "/day03/input.txt"
-    val input = object {}::class.java.getResource(file)?.readText()
-    checkNotNull(input) { "cannot read input from $file" }
+    val input = FileUtil.readFile("/day03/input.txt")
 
     val sum = Multiplier.multiply(input)
     println("sum of all products: $sum")
--- src/main/kotlin/day04/Main.kt
+++ src/main/kotlin/day04/Main.kt
@@ -1,9 +1,9 @@
 package day04
 
+import util.FileUtil
+
 fun main() {
-    val file = "/day04/input.txt"
-    val input = object {}::class.java.getResource(file)?.readText()
-    checkNotNull(input) { "cannot read input from $file" }
+    val input = FileUtil.readFile("/day04/input.txt")
 
     val sum = WordSearch.count(input, "XMAS")
     println("sum of all XMAS words: $sum")
