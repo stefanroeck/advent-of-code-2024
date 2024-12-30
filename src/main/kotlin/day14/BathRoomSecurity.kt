e2c22eeaa90cca390d283402555d7826256bd86c
Stefan Roeck <stefan.roeck@gmail.com>
2024-12-30 23:42:00 +0100

    day14 part 2

--- src/main/kotlin/day14/BathRoomSecurity.kt
+++ src/main/kotlin/day14/BathRoomSecurity.kt
@@ -13,6 +13,26 @@ private data class Quadrant(val topLeft: Point, val bottomRight: Point) {
     infix fun contains(point: Point) = point.within(topLeft, bottomRight)
 }
 
+private data class Quadrants(val width: Long, val height: Long) {
+
+    val middleRow: Long
+        get() =
+            height / 2
+
+    val middleCol: Long
+        get() =
+            width / 2
+
+    val quadrants: List<Quadrant>
+        get() =
+            listOf(
+                Quadrant(topLeft = Point(0, 0), bottomRight = Point(middleCol - 1, middleRow - 1)),
+                Quadrant(topLeft = Point(middleCol + 1, 0), bottomRight = Point(width - 1, middleRow - 1)),
+                Quadrant(topLeft = Point(0, middleRow + 1), bottomRight = Point(middleCol - 1, height - 1)),
+                Quadrant(topLeft = Point(middleCol + 1, middleRow + 1), bottomRight = Point(width - 1, height - 1)),
+            )
+}
+
 private val robotLineRegex = Regex("""^p=([-\d]+),([-\d]+) v=([-\d]+),([-\d]+)$""")
 
 class BathRoomSecurity(private val lines: List<String>, private val width: Long, private val height: Long) {
@@ -35,25 +55,67 @@ class BathRoomSecurity(private val lines: List<String>, private val width: Long,
         check(width % 2L == 1L) { "width is expected to be uneven" }
         check(height % 2L == 1L) { "height is expected to be uneven" }
 
-        val middleRow = height / 2L
-        val middleCol = width / 2L
-        val quadrants = listOf(
-            Quadrant(topLeft = Point(0, 0), bottomRight = Point(middleCol - 1, middleRow - 1)),
-            Quadrant(topLeft = Point(middleCol + 1, 0), bottomRight = Point(width - 1, middleRow - 1)),
-            Quadrant(topLeft = Point(0, middleRow + 1), bottomRight = Point(middleCol - 1, height - 1)),
-            Quadrant(topLeft = Point(middleCol + 1, middleRow + 1), bottomRight = Point(width - 1, height - 1)),
-        )
+        val bathRoom = Quadrants(width, height)
 
         val robotsByQuadrants = robots
-            .filter { it.position.row != middleRow && it.position.col != middleCol }
+            .filter { it.position.row != bathRoom.middleRow && it.position.col != bathRoom.middleCol }
             .groupBy {
-                quadrants.single { q ->
+                bathRoom.quadrants.single { q ->
                     q contains it.position
                 }
             }
 
         // multiple count of each quadrant
         return robotsByQuadrants.entries.map { it.value.size }.fold(1) { prev, curr -> prev * curr }
+    }
+
+    fun findChristmasTree(startAtSteps: Int = 1000, stopAtSteps: Int = 10000) {
+        var loops = 0
+        val robots = parseRobotPositions(lines)
+        (0..<startAtSteps).forEach { _ ->
+            robots.forEach { moveRobot(it) }
+            loops++
+        }
+
+        val blankLine = ".".repeat(width.toInt())
+        val suspicousLines = mutableMapOf<Int, Int>()
+
+        (startAtSteps..stopAtSteps).forEach { idx ->
+            loops++
+            robots.forEach { moveRobot(it) }
+            // look for symmetries, as a x-mas tree is expected to be symmetric ;-)
+            val suspiciousLines = (0..<height).filter { row ->
+                val line = (0..<width).joinToString("") { col ->
+                    val count = robots.count { it.position == Point(col, row) }
+                    if (count > 0) "x" else "."
+                }
+                if (line != blankLine) {
+                    line.contains("x".repeat(7))
+                } else {
+                    false
+                }
+            }
+
+            if (suspiciousLines.count() > 3) {
+                println("Found easter egg after $loops seconds: $suspiciousLines")
+                printRobotPositions(robots)
+                suspicousLines.put(loops, suspiciousLines.size)
+            }
+        }
+
+        val sortedEntries = suspicousLines.toList().sortedByDescending { (_, value) -> value }.toMap()
+        println(sortedEntries)
+    }
+
+    private fun printRobotPositions(robots: List<Robot>) {
+        println("")
+        (0..<height).forEach { row ->
+            (0..<width).forEach { col ->
+                val robotCount = robots.count { it.position == Point(col, row) }
+                print(if (robotCount > 0) "x" else ".")
+            }
+            print("\n")
+        }
     }
 
     private fun moveRobot(robot: Robot) = robot.moveOnMap(this.width, this.height)
--- src/main/kotlin/day14/Main.kt
+++ src/main/kotlin/day14/Main.kt
@@ -7,7 +7,11 @@ fun main() {
     val input = FileUtil.readFile("/day14/input.txt")
     val rows = parseLines(input)
 
-    val safetyFactor = BathRoomSecurity(rows, width = 101, height = 103).predictMovement(100)
+    val bathRoomSecurity = BathRoomSecurity(rows, width = 101, height = 103)
+
+    val safetyFactor = bathRoomSecurity.predictMovement(1000)
     println("Safety factor in front of the bath room: $safetyFactor")
+
+    bathRoomSecurity.findChristmasTree(startAtSteps = 7583, stopAtSteps = 7583)
 
 }
